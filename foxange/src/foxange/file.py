20ca313c87435f8482eac41dbc6dbb61502d86fc
foxange-org <slc871205abc@outlook.com>
2026-05-03 10:31:03 +0800

    Delete foxange directory

--- foxange/src/foxange/file.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-def input_to_file():pass
-
-if __name__ == "__main__":
-    print("this is a test file.py 's def , if you want use there , don't open the there , use 'import foxange'")
-    print("down is test file.py")
-    print("i will print can return value 's def and all def 's return path")
